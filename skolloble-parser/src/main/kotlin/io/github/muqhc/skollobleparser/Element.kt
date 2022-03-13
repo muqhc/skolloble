4ea7e66e4d720422a0550fe189afbfd7906db7d0
muqhc <muqhc07@gmail.com>
2022-03-13 18:21:39 +0900

    fix bug on read String Null Pointer Exception

--- skolloble-parser/src/main/kotlin/io/github/muqhc/skollobleparser/Element.kt
+++ skolloble-parser/src/main/kotlin/io/github/muqhc/skollobleparser/Element.kt
@@ -5,7 +5,7 @@ import io.github.muqhc.skollobleparser.raw.SkollobleParser
 class Element(private val elementContext: SkollobleParser.ElementContext) {
     val name = elementContext.name().ID().text
     val namespace = elementContext.name()?.forenamespace()?.text ?: elementContext.name()?.backnamespace()?.text
-    val children: List<Element> = elementContext.block()?.element()?.filter { it.name().ID() != null } ?.map { Element(it) } ?: emptyList()
+    val children: List<Element> = elementContext.block()?.element()?.filter { it.name() != null } ?.map { Element(it) } ?: emptyList()
     val strings: List<String> = elementContext.block()?.element()?.filter { it.STRING() != null } ?.map { it.STRING().text.drop(1).dropLast(1) } ?: emptyList()
     val attribution: Attribution = Attribution(elementContext.attribution())
 }
